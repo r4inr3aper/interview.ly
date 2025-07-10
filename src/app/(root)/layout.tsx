155e1eb2d2dc4732475ca1f951020d05e6e498e0
bedanta <r4inr3aper@gmail.com>
2025-07-10 21:37:07 +0530

    initial commit

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+const RootLayout = ( { children }: { children: React.ReactNode } ) => {
+  return (
+    <div>RootLayout</div>
+  )
+}
+
+export default RootLayout
